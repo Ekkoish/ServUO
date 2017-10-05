c7e1b832d4143c4172c9cfcd9bce1cf5a2499e99
Ekkoish <32531462+Ekkoish@users.noreply.github.com>
2017-10-05 14:27:53 -0500

    Add files via upload

--- Scripts/Custom/New Items/Sheaths/EmptyWaistSheath.cs
+++ Scripts/Custom/New Items/Sheaths/EmptyWaistSheath.cs
@@ -1,24 +1,19 @@
 using System;
+using Server.Mobiles;
 
 namespace Server.Items
 {
-	public class Fur : Item
+	public class EmptyWaistSheath : Item
 	{
 		[Constructable]
-		public Fur() : this( 1 )
+		public EmptyWaistSheath() : base( 0x3B35 )
 		{
-		}
-		
-		[Constructable]
-		public Fur( int amount ) : base( 0x11F5 )
-		{
+			Name = "empty waist sheath";
 			Weight = 1.0;
-            Name = "fur";
-            Stackable = true;
-            Amount = amount;
+			Layer = Layer.Invalid;
 		}
 
-		public Fur( Serial serial ) : base( serial )
+		public EmptyWaistSheath( Serial serial ) : base( serial )
 		{
 		}
 
@@ -26,7 +21,7 @@ namespace Server.Items
 		{
 			base.Serialize( writer );
 
-			writer.Write( (int) 0 );
+			writer.Write( (int) 0 ); // version
 		}
 
 		public override void Deserialize( GenericReader reader )
